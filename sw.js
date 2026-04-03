8887c9532fdd1a7c9b95e774857c8224d4ff2939
vasanth-medapati <veeravasanth.medapati@gmail.com>
2026-04-03 13:43:35 +0530

    Implement service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'trailbharat-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/data.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        })
+        .map(cacheName => {
+          console.log('Deleting old cache:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (!response || response.status !== 200 || response.type === 'error') {
+          return response;
+        }
+
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(response => {
+            return response || new Response('Offline - Content not available');
+          });
+      })
+  );
+});
